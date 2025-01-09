390d785238f96cbf2dd46fd9df20ae21a585fc83
KebebGinuk <cristianaloysius@gmail.com>
2025-01-09 13:25:06 +0700

    Coba push

---
+++ Controller/DatabaseHandler.java
@@ -0,0 +1,53 @@
+package Controller;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+public class DatabaseHandler extends SQLException{
+
+    public static Connection con;
+    private final static String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private final static String URL = "jdbc:mysql://localhost/uas_pbo";
+    // private static String url = "jdbc:mysql://localhost/db_test?serverTimezone=" + TimeZone.getDefault().getID();
+    private final static String USERNAME = "root";
+    private final static String PASSWORD = "";
+
+    private static Connection logOn() {
+        try {
+            Class.forName(DRIVER).newInstance();
+            con = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (Exception ex) {
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getLocalizedMessage());
+            JOptionPane.showMessageDialog(null, "Error Ocurred when login" + ex);
+        }
+        return con;
+    }
+    private static void logOff() {
+        try {
+            con.close();
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(null, "Error Ocurred when login" + ex);
+        }
+    }
+
+    public static Connection connect() {
+        try {
+            con = logOn();
+            System.out.println("Koneksi Berhasil");
+        } catch (Exception ex) {
+            System.out.println("Error occured when connecting to database");
+        }
+        return con;
+    }
+
+    public static void disconnect() {
+        try {
+            logOff();
+        } catch (Exception ex) {
+            System.out.println("Error occured when connecting to database");
+        }
+    }
+}
